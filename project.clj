b658dff612d438da90c59761ea522d34339e8064
Laurent Petit <laurent.petit@gmail.com>
2011-02-03 07:17:11 +0100

    mavenization

--- project.clj
+++
@@ -1,4 +0,0 @@
-(defproject net.cgrand/regex "1.0.0-SNAPSHOT"
-  :description "a DSL for people who prefer verbose, composable regexes"
-  :url "http://github.com/cgrand/regex/"
-  :dependencies [[org.clojure/clojure "[1.2.0,)"]])
